c208f4f94c852f4333fad2558fe940a3ec2c015d
Olga G <oldchatot@gmail.com>
2023-06-15 14:04:21 +0200

    actualizacion

--- html/database/migrations/2014_10_12_000000_create_users_table.php
+++ html/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('username');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at');
-            $table->string('thumbnail')->nullable();
-            $table->string('profile')->nullable();
-            $table->boolean('is_privated')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
