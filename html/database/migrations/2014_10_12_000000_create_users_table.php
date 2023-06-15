9bf2faf15ad652bb5f766801520d421ba81a4d5a
Sjuniperus <sapindacea@gmail.com>
2023-06-15 13:27:56 +0200

    first commit

---
+++ html/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at');
+            $table->string('thumbnail')->nullable();
+            $table->string('profile')->nullable();
+            $table->boolean('is_privated')->default(0);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
