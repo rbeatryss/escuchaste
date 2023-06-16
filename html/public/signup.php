f9aaafd4cc20641c63be4b6d802e3bf7d23eb7af
rbeatryss <127734201+rbeatryss@users.noreply.github.com>
2023-06-16 11:59:43 +0200

    lay out de log in y sign up, pendiente: validaciones

---
+++ html/public/signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+    <link rel="stylesheet" href="signup.css">
+</head>
+<body>
+    <div class="logo">
+        <img src="img/logo_extended.svg" alt="logo">
+    </div>
+    <h1>Create new account</h1>
+        <div class="signInContainer">
+            <form action="#" method="post">
+
+                <label for="password">name</label>
+                <input type="text" id="name" name="name">
+
+                <label for="password">surname</label>
+                <input type="text" id="fname" name="fname">
+
+                <label for="username">username</label>
+                <input type="text" id="username" name="username">
+
+                <label for="email">email</label>
+                <input type="text" id="email" name="email">
+
+                <label for="email">email confirmation</label>
+                <input type="text" id="email" name="email">
+
+                <label for="password">password</label>
+                <input type="password" id="password" name="password">
+
+                <label for="password">password confirmation</label>
+                <input type="password" id="password" name="password">
+
+                <input class="btnSubmit" type="submit" value="Sign up">
+            </form>
+        </div>
+</body>
+</html>
