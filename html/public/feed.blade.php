800abf62f90593057140ee65943ba39ae2ca262e
rbeatryss <127734201+rbeatryss@users.noreply.github.com>
2023-06-20 11:55:10 +0200

    merge required

--- html/public/feed.blade.php
+++ html/public/feed.blade.php
@@ -7,8 +7,39 @@
     <link rel="stylesheet" href="feed.css">
 </head>
 <body>
-<div class="navbar">
-    <a href="home.blade.php"
+<div class="navbarLeft">
+    <div class="welcome">
+        <img/>
+        <span class="welcomeUser">Welcome,user</span>
+        <a href="#" class="signInSignUp"></a>
+    </div>
+
+    <ul class="navbar">
+        <a href="home.blade.php"><img src="img/icon_home_after.svg" alt="icon_home">Home</a>
+        <a href="saved.blade.php"><img src="img/icon_saved.svg" alt="icon_saved">Saved</a>
+        <a href="settings.blade.php"><img src="img/icon_settings.svg" alt="icon_settings">Settings</a>
+    </ul>
 </div>
+
+<!-- <div class="navbar">
+    <div class="home">
+    <a href="home.blade.php">
+        <img src="img/icon_home_after.svg" alt="icon_home">
+    </a>
+
+    </div>
+    <div class="saved">
+    <a href="saved.blade.php">
+        <img src="img/icon_saved.svg" alt="icon_saved">
+    </a>
+
+    </div>
+    <div class="settings">
+    <a href="settings.blade.php">
+        <img src="img/icon_settings.svg" alt="icon_settings">
+    </a>
+
+    </div>
+</div> -->
 </body>
 </html>
